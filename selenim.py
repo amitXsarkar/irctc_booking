935f68f50036f2eed08000c990ad4d5ec797bfde
Lalitha <lalithaeleswarapu98@gmail.com>
2019-05-26 14:43:31 +0530

    Modified 2.0

--- selenim.py
+++ selenim.py
@@ -58,7 +58,49 @@ def check(word, book_list):
 
 check('RAC',book_list)
 time.sleep(5)
-book_now.send_keys('rameleswar')
+login_Id = browser.find_element_by_id('userId')
+login_Id.send_keys('rameleswar')
+login_Id.send_keys(Keys.TAB)
+
+login_pw = browser.find_element_by_id('pwd')
+login_pw.send_keys('nsso(fod)')
+login_pw.send_keys(Keys.TAB)
+
+time.sleep(10)
+
+
+captcha = browser.find_element_by_id('nlpAnswer')
+captcha.send_keys("Invesco")
+sign_in = browser.find_element_by_xpath('//button[@class="search_btn"]')
+# captcha.send_keys(Keys.RETURN)
+
+login_pw.click()
+login_pw.send_keys(Keys.RETURN)
+
+
+time.sleep(20)
+
+
+
+psgn_name = "LAlitha"
+psgn_age = "21"
+psgn_gender = "female"
+
+psgn = browser.find_element_by_id('psgn-name')
+psgn = send_keys(psgn_name)
+
+
+psgn.send_keys(Keys.Tab)
+psgn.send_keys(psgn_age)
+psgn.send_keys(Keys.Tab)
+
+if psgn_gender == "Male":
+    psgn.send_keys(Keys.ARROW_DOWN)
+elif psgn_gender == "Female":
+    psgn.send_keys(Keys.ARROW_DOWN)
+    psgn.send_keys(Keys.ARROW_DOWN)
+
+
 # book_now.send_keys(Keys.RETURN)
 # book_now.send_keys('nsso(fod)')
 # book_now.send_keys(Keys.RETURN)
